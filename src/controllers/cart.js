5ba29ca0361a63fbbea5ed62ebd4a94b9ee769b0
Duyy2707 <duy27072003@gmail.com>
2023-10-01 13:31:21 +0700

    fix: update cart

--- src/controllers/cart.js
+++ src/controllers/cart.js
@@ -1,10 +1,6 @@
 import Cart from '../models/cart.js'
-import Auth from "../models/auth.js";
-import mongoose from 'mongoose'
-import { cartSchema } from '../schemas/cart.js';
-import Coupon from "../models/coupons.js"
-import Order from "../models/order.js"
-import Product from "../models/product.js";
+import User from "../models/auth.js";
+import { ProductSchema, productCart } from '../schemas/product.js'
 
 
 export const resetCart = async (idUser) => {
@@ -20,94 +16,42 @@ export const resetCart = async (idUser) => {
     }
 }
 
-
 const addProduct = async (cartExist, productAdd, res) => {
     try {
-        const productExist = cartExist.products.find((product) =>
-            product.productId === productAdd.productId &&
-            product.sizeId === productAdd.sizeId &&
-            product.colorId === productAdd.colorId
-        );
+        const productExist = cartExist.products.find((product) => product.productId === productAdd.productId)
         if (productExist) {
-            const size = await Size.findById(productAdd.sizeId);
-            const updatedProductPrice = productAdd.product_price + size.size_price;
-            productExist.stock_quantity += productAdd.stock_quantity;
-            productExist.product_price = updatedProductPrice; // Cập nhật giá sản phẩm
-
-            cartExist.total += productAdd.stock_quantity * updatedProductPrice;
+            productExist.quantity += productAdd.quantity
+            cartExist.total += productAdd.quantity * productAdd.price
         } else {
-            const size = await Size.findById(productAdd.sizeId)
-            const newProduct = {
-                productId: productAdd.productId,
-                product_name: productAdd.product_name,
-                product_price: productAdd.product_price + size.size_price,
-                image: productAdd.image,
-                stock_quantity: productAdd.stock_quantity,
-                sizeId: productAdd.sizeId,
-                colorId: productAdd.colorId
-            }
-            cartExist.products.push(newProduct);
-            cartExist.total += productAdd.stock_quantity * newProduct.product_price;
+            cartExist.products.push(productAdd)
+            cartExist.total += productAdd.quantity * productAdd.price
         }
-        for (const item of cartExist.products) {
-            const product = await Product.findById(item.productId);
-
-            if (!product || product.stock_quantity < item.stock_quantity) {
-                return res.status(400).json({ message: `Đã quá số hàng tồn` });
-            }
-        }
-
-        // Kiểm tra xem phiếu giảm giá đã được áp dụng
-        if (cartExist.couponId !== null) {
-            // Tính lại tổng tiền bằng cách cộng với giá của sản phẩm mới
-            cartExist.products = cartExist.products.map((item) => {
-                return {
-                    ...item,
-                    product_price: item.originalPrice, // Khôi phục giá của sản phẩm về giá ban đầu
-                };
-            });
-            cartExist.total = cartExist.products.reduce((total, item) => total + item.product_price * item.stock_quantity, 0);
-            cartExist.couponId = null
-            // Xóa giá ban đầu của sản phẩm
-            cartExist.products.forEach((item) => {
-                item.originalPrice = undefined
-            });
-        }
-        // Lưu giỏ hàng sau khi cập nhật (không có phiếu giảm giá)
         const cartUpdated = await Cart.findOneAndUpdate({ _id: cartExist._id }, cartExist, { new: true })
         return res.status(200).json({
             message: 'Thêm vào giỏ hàng thành công',
             data: cartUpdated
-        });
+        })
     } catch (error) {
-        console.log(error.message);
-        return res.status(400).json({
+        console.log(error.message)
+        return res.status(404).json({
             message: 'Thêm vào giỏ hàng không thành công'
-        });
+        })
     }
-};
-
-
-
+}
 export const create = async (req, res) => {
     try {
         const userId = req.params.id
         const productNeedToAdd = req.body
-        const userExist = await Auth.findById(userId);
-        const product = await Product.findById(productNeedToAdd.productId);
+        const userExist = await User.findById(userId)
         if (!userExist) {
             return res.status(404).json({
                 message: 'Người dùng không tồn tại !'
             })
         }
-        if (!product || product.stock_quantity < productNeedToAdd.stock_quantity) {
-            return res.status(400).json({ message: `Đã quá số hàng tồn` });
-        }
-        const { error } = cartSchema.validate(req.body, { abortEarly: false });
+        const { error } = productCart.validate(req.body)
         if (error) {
-            const errors = error.details.map((err) => err.message);
             return res.status(400).json({
-                message: errors
+                message: error.details[0].message
             })
         }
         const cartExist = await Cart.findOne({ userId: userId })
@@ -122,14 +66,14 @@ export const create = async (req, res) => {
                     ...productNeedToAdd
                 }
             ],
-            total: productNeedToAdd.product_price * productNeedToAdd.stock_quantity,
+            total: productNeedToAdd.price * productNeedToAdd.quantity
         })
         if (!newCart) {
-            return res.status(400).json({
-                message: 'Thêm vào giỏ hàng thất bại'
+            return res.json({
+                message: 'Thêm vào giỏ hàng không thành công'
             })
         }
-        return res.status(200).json({
+        return res.json({
             message: 'Thêm vào giỏ hàng thành công',
             data: newCart
         })
@@ -139,18 +83,16 @@ export const create = async (req, res) => {
         })
     }
 }
-
-
 export const getOne = async (req, res) => {
     try {
         const cart = await Cart.findOne({ userId: req.params.id })
         if (!cart) {
-            return res.status(404).json({
+            return res.json({
                 message: 'Không tìm thấy giỏ hàng',
                 data: []
             })
         }
-        return res.status(200).json({
+        res.json({
             message: 'Lấy giỏ hàng thành công',
             data: cart
         })
@@ -160,26 +102,24 @@ export const getOne = async (req, res) => {
         })
     }
 }
-
-
 export const changeQuantity = async (req, res) => {
     try {
         const idUser = req.params.id
         const { idProduct = '' } = req.query
-        const { stock_quantity } = req.body
-        const userExist = await Auth.findOne({ _id: idUser })
+        const { quantity } = req.body
+        const userExist = await User.findOne({ _id: idUser })
         if (!userExist) {
-            return res.status(404).json({
+            return res.json({
                 message: 'Vui lòng đăng nhập!'
             })
         }
         const cart = await Cart.findOne({ userId: idUser })
         const productExt = cart.products.find((product) => product.productId === idProduct)
         if (productExt) {
-            productExt.stock_quantity = stock_quantity
+            productExt.quantity = quantity
             const productsUpdated = [...cart.products.filter((product) => product.productId !== idProduct), productExt]
             const totalUpdated = productsUpdated.reduce((total, product) => {
-                return (total += product.stock_quantity * product.product_price)
+                return (total += product.quantity * product.price)
             }, 0)
             const cartUpdated = await Cart.findOneAndUpdate(
                 { userId: idUser },
@@ -187,12 +127,12 @@ export const changeQuantity = async (req, res) => {
                 { new: true }
             )
 
-            return res.status(200).json({
+            return res.json({
                 message: 'Thay đổi sản phẩm thành công',
                 data: cartUpdated
             })
         }
-        return res.status(400).json({
+        return res.json({
             message: 'Sản phẩm không tồn tại!',
             data: {}
         })
@@ -202,29 +142,27 @@ export const changeQuantity = async (req, res) => {
         })
     }
 }
-
-
 export const removeProduct = async (req, res) => {
     try {
         const idUser = req.params.id
         const { idProduct = '' } = req.query
-        const userExist = await Auth.findOne({ _id: idUser })
+        const userExist = await User.findOne({ _id: idUser })
         if (!userExist) {
-            return res.status(404).json({
+            return res.json({
                 message: 'Vui lòng đăng nhập!'
             })
         }
         const cart = await Cart.findOne({ userId: idUser })
         const productsUpdated = cart.products.filter((product) => product.productId !== idProduct)
         const totalUpdated = productsUpdated.reduce((total, product) => {
-            return (total += product.stock_quantity * product.product_price)
+            return (total += product.quantity * product.price)
         }, 0)
         const cartUpdated = await Cart.findOneAndUpdate(
             { userId: idUser },
             { $set: { products: productsUpdated, total: totalUpdated } },
             { new: true }
         )
-        return res.status(200).json({
+        res.json({
             message: 'Xóa sản phẩm thành công',
             data: cartUpdated
         })
@@ -234,169 +172,3 @@ export const removeProduct = async (req, res) => {
         })
     }
 }
-
-export const clearUserCart = async (req, res) => {
-    try {
-        const userId = new mongoose.Types.ObjectId(req.params.id);
-        const cartExist = await Cart.findOne({ userId });
-
-        if (!cartExist) {
-            return res.status(404).json({ message: 'Không tìm thấy giỏ hàng cho người dùng này' });
-        }
-
-        cartExist.products = []; // Xoá tất cả sản phẩm trong giỏ hàng
-        cartExist.total = 0;// Đặt tổng giá trị về 0
-        cartExist.couponId = null
-        const cartUpdated = await Cart.findOneAndUpdate({ _id: cartExist._id }, cartExist, { new: true });
-
-        return res.status(200).json({
-            message: 'Xoá tất cả sản phẩm trong giỏ hàng thành công',
-            data: cartUpdated,
-        });
-    } catch (error) {
-        console.error(error); // In ra thông tin lỗi cụ thể
-        return res.status(500).json({ message: 'Xoá tất cả sản phẩm trong giỏ hàng không thành công' });
-    }
-};
-
-// Áp dụng mã phiếu giảm giá vào giỏ hàng
-const applyCouponToCart = async (userId, couponId) => {
-    try {
-        const cart = await Cart.findOne({ userId });
-        const coupon = await Coupon.findById(couponId);
-        if (coupon.discount_amount) {
-            // Lưu trữ giá ban đầu của từng sản phẩm trong giỏ hàng
-            cart.products = cart.products.map((item) => {
-                return {
-                    ...item,
-                    originalPrice: item.product_price, // Lưu giá ban đầu của sản phẩm
-                };
-            });
-            cart.products = cart.products.map((item) => {
-                const originalItemPrice = item.product_price;
-                const discountAmount = (coupon.discount_amount / 100) * originalItemPrice;
-                const discountedPrice = originalItemPrice - discountAmount;
-                return {
-                    ...item,
-                    product_price: discountedPrice,
-                };
-            });
-            // Cập nhật giỏ hàng với giảm giá
-            cart.total = cart.products.reduce((total, item) => total + item.product_price * item.stock_quantity, 0);
-            // Đánh dấu giỏ hàng đã áp dụng mã phiếu giảm giá
-            cart.couponId = coupon._id;
-
-            // Lưu giỏ hàng sau khi cập nhật
-            const updatedCart = await cart.save();
-
-            return updatedCart;
-        }
-
-        return cart;
-    } catch (error) {
-        throw error;
-    }
-};
-
-// Sử dụng hàm applyCouponToCart trong route handler
-export const applyCoupon = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const couponId = req.body.couponId;
-
-        const cart = await Cart.findOne({ userId });
-        if (!cart) {
-            return res.status(404).json({ message: 'Không tìm thấy giỏ hàng cho người dùng này' });
-        }
-        if (cart.couponId != null) {
-            return res.status(404).json({ message: 'Chỉ được sử dụng 1 phiếu giảm giá' });
-        }
-
-        const coupon = await Coupon.findById(couponId);
-        if (cart.total < coupon.min_purchase_amount) {
-            return res.status(404).json({ message: 'Không đủ điều kiện để sử dụng phiếu giảm giá' })
-        }
-        if (!coupon) {
-            return res.status(404).json({ message: 'Mã phiếu giảm giá không hợp lệ' });
-        }
-        // check xem người dùng đã sử dụng phiếu giảm giá này để order hay chưa
-        const orderWithCoupon = await Order.findOne({ couponId: couponId });
-        if (orderWithCoupon) {
-            return res.status(400).json({ message: 'Phiếu giảm giá đã được sử dụng' });
-        }
-
-        // Kiểm tra xem phiếu giảm giá có quá hạn không
-        const currentDate = new Date();
-        if (currentDate > coupon.expiration_date) {
-            return res.status(400).json({ message: 'Mã phiếu giảm giá đã hết hạn' });
-        }
-
-        // Áp dụng mã phiếu giảm giá vào giỏ hàng bằng cách gọi hàm applyCouponToCart
-        const updatedCart = await applyCouponToCart(userId, couponId);
-
-        return res.json({
-            message: 'Áp dụng phiếu giảm giá thành công',
-            data: updatedCart,
-        });
-    } catch (error) {
-        return res.status(500).json({ message: 'Có lỗi xảy ra khi áp dụng phiếu giảm giá' });
-    }
-};
-
-// Hủy bỏ sử dụng mã phiếu giảm giá
-const removeCouponFromCart = async (userId) => {
-    try {
-        const cart = await Cart.findOne({ userId });
-        // Kiểm tra xem có mã phiếu giảm giá đã được áp dụng không
-        if (cart.couponId) {
-            // Khôi phục giá của từng sản phẩm từ giá ban đầu
-            cart.products = cart.products.map((item) => {
-                return {
-                    ...item,
-                    product_price: item.originalPrice, // Khôi phục giá của sản phẩm về giá ban đầu
-                };
-            });
-
-            // Đặt couponId về null để đánh dấu là không sử dụng mã phiếu giảm giá
-            cart.couponId = null;
-
-            // Tính toán lại tổng giá trị của giỏ hàng sau khi khôi phục giá sản phẩm
-            cart.total = cart.products.reduce((total, item) => total + item.product_price * item.stock_quantity, 0);
-
-            // Xóa giá ban đầu của sản phẩm
-            cart.products.forEach((item) => {
-                item.originalPrice = undefined
-            });
-            // Lưu giỏ hàng sau khi cập nhật
-            const updatedCart = await cart.save();
-
-            return updatedCart;
-        }
-
-        return cart;
-    } catch (error) {
-        throw error;
-    }
-};
-
-
-// Sử dụng hàm removeCouponFromCart trong route handler để huỷ bỏ sử dụng mã phiếu giảm giá
-export const removeCoupon = async (req, res) => {
-    try {
-        const userId = req.params.id;
-        const cart = await Cart.findOne({ userId });
-        if (!cart) {
-            return res.status(400).json({ message: 'Không tìm thấy giỏ hàng cho người dùng này' });
-        }
-        if (cart.couponId == null) {
-            return res.status(400).json({ message: 'Không sử dụng phiếu giảm giá' });
-        }
-        const updatedCart = await removeCouponFromCart(userId);
-        return res.json({
-            message: 'Huỷ bỏ sử dụng phiếu giảm giá thành công',
-            data: updatedCart,
-        });
-    } catch (error) {
-        return res.status(500).json({ message: 'Có lỗi xảy ra khi huỷ bỏ sử dụng phiếu giảm giá' });
-    }
-};
--- src/routers/cart.js
+++ src/routers/cart.js
@@ -1,15 +1,9 @@
-import express from "express";
-import { applyCoupon, changeQuantity, clearUserCart, create, getOne, removeCoupon, removeProduct } from "../controllers/cart.js";
+import express from 'express'
+import { changeQuantity, create, getOne, removeProduct } from '../controllers/cart.js'
 
-const cartRouter = express.Router();
-
-cartRouter.get("/carts/:id", getOne);
-cartRouter.post("/carts/:id/create", create);
-cartRouter.delete("/carts/:id/remove", removeProduct);
-cartRouter.delete("/carts/:id/clears", clearUserCart);
-cartRouter.put("/carts/:id/change", changeQuantity);
-cartRouter.patch("/carts/:id/apply", applyCoupon);
-cartRouter.patch("/carts/:id/remove-coupon", removeCoupon);
-
-
-export default cartRouter;
+const routerCart = express.Router()
+routerCart.get("/cart/:id", getOne)
+routerCart.post("/cart/:id", create)
+routerCart.delete("/cart/:id", removeProduct)
+routerCart.patch("/cart/:id", changeQuantity)
+export default routerCart
